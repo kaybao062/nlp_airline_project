a3fd8d488bb7d1d52d45cc2c9b91f86fbccde954
Peter Pezon <pezon@uchicago.edu>
2024-05-09 23:09:56 -0500

    fix build index script

--- scripts/build-policies-index.py
+++ scripts/build-policies-index.py
@@ -47,41 +47,24 @@ def build_index(name: str):
     # load store:
     policy_config = load_config(name)
     storage_context = load_store_context(policy_config)
-
-    # initialize empty index:
-    index: KnowledgeGraphIndex = None
-
-    # load airlines:
-    policies = load_policies(as_dataframe=True, limit=1000)
-    airlines = policies["Airline"].unique()
-    print(f"Found airlines: {airlines}")
-
-    # iterate through airlines, filter policies grouped by airlines,
-    # and insert policy documents into index in batches
-    for airline in airlines:
-        print(f"Loading policies: {airline}")
-        airline_policies = load_policies(airline=airline)
-
-        # insert documents to index
-        if index is None:
-            index = KnowledgeGraphIndex.from_documents(
-                airline_policies,
-                storage_context=storage_context,
-                max_triplets_per_chunk=20,
-                space_name=policy_config.space_name,
-                edge_types=policy_config.edge_types,
-                rel_prop_names=policy_config.rel_prop_names,
-                tags=policy_config.tags,
-                include_embeddings=True,
-                # kg_triplet_extract_fn=extract_triplets,
-                verbose=True,
-            )
-        else:
-            index.insert(airline_policies, verbose=True)
-        
-        print(f"Persisting policies: {policy_config.persist_dir}")
-        index.storage_context.persist(persist_dir=policy_config.persist_dir)
-
+    # load documents:
+    policies = load_policies(limit=1000)
+    # build index:
+    index = KnowledgeGraphIndex.from_documents(
+        policies,
+        storage_context=storage_context,
+        max_triplets_per_chunk=20,
+        space_name=policy_config.space_name,
+        edge_types=policy_config.edge_types,
+        rel_prop_names=policy_config.rel_prop_names,
+        tags=policy_config.tags,
+        include_embeddings=True,
+        # kg_triplet_extract_fn=extract_triplets,
+        verbose=True,
+    )
+    # persist index:
+    print(f"Persisting policies: {policy_config.persist_dir}")
+    index.storage_context.persist(persist_dir=policy_config.persist_dir)
     print("Done!")
 
 
