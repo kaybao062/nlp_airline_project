1c7d50d8b521d7f3788fc84b686d75dc4e34c98d
kaybao062 <116699898+kaybao062@users.noreply.github.com>
2024-05-22 19:40:27 -0500

    Update vector.py

--- util/vector.py
+++ util/vector.py
@@ -1,4 +1,14 @@
-!pip install pinecone-client
+import pip
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+# Example
+if __name__ == '__main__':
+    install('pinecone-client')
 import logging
 import sys
 import os
